5bbfed9deae637b0ca9432d9a2e75dc8c5311d20
yrmint <zhilkinal@yandex.ru>
2024-04-30 23:29:27 +0300

    fix offer id type

--- src/store/page-events/selectors.ts
+++ src/store/page-events/selectors.ts
@@ -2,4 +2,4 @@ import { SlicesName } from '../../const/const';
 import { State } from '../../types/state';
 
 export const getSortType = (state: State): string => state[SlicesName.Page].sortType;
-export const getCurrentOfferId = (state: State): number | null => state[SlicesName.Page].currentOfferId;
+export const getCurrentOfferId = (state: State): string | null => state[SlicesName.Page].currentOfferId;
